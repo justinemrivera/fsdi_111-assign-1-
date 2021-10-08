e8cab477e232b3c49decb8ae462ee698d133bd54
Justine Rivera <justineee.m.25@gmail.com>
2021-10-07 20:22:11 -0700

    1st assignment

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask  # From the flask package import the Flask class
+
+# instaniate the Flask class into app(which is now an obj)
+app = Flask(__name__)
+
+
+@app.route("/")  # decorater that maps the path to a view function
+def index():               # a view function called index
+    return "<h1> Hello, world!</h1>"  # return statement for index
+
+
+@app.route("/about")
+def about_me():
+    me = {
+        "first_name": "Justine",
+        "last_name": "Rivera",
+        "hobbies": "DIY STUFF",
+        "active": True,
+    }
+    return me
